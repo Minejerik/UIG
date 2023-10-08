2198bf0276bbfa39d1d97d58a64bbbba2e6f54af
minejerik <jaanerikf@gmail.com>
2023-10-08 14:19:30 -0400

    we do a little

---
+++ src/main/java/dev/minejerik/itemplugin/item_test2/Item_test2.java
@@ -0,0 +1,17 @@
+package dev.minejerik.itemplugin.item_test2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Item_test2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
